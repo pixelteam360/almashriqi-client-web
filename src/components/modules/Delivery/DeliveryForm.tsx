833e842fb86a045e8c6c02a1b7811dc6f95d5385
Shahin Alam <shahinexy@gmail.com>
2025-05-15 11:55:32 +0600

    push

--- src/components/modules/Delivery/DeliveryForm.tsx
+++ src/components/modules/Delivery/DeliveryForm.tsx
@@ -1,27 +1,85 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
-
+import { Autocomplete, useLoadScript } from "@react-google-maps/api";
 import MyBtn from "@/components/common/MyBtn";
 import MyFormInput from "@/components/form/MyFormInput";
 import MyFormSelect from "@/components/form/MyFormSelect";
 import MyFormWrapper from "@/components/form/MyFormWrapper";
 import { serviceType, timeType } from "@/constants/common";
 import { useDeliveryMutation } from "@/redux/features/common/commonApi";
-import { validateHHMMTime } from "@/utils/validateHHMMTime";
 import { useRouter, useSearchParams } from "next/navigation";
+import { useRef, useState } from "react";
 import { FieldValues } from "react-hook-form";
 import { toast } from "sonner";
+import Spinner from "@/components/common/Spinner";
 
 const DeliveryForm = () => {
   const router = useRouter();
   const price = useSearchParams().get("price");
   const [delivery] = useDeliveryMutation();
+  const [isMultiple, setIsMultiple] = useState(false);
+  const [pickup, setPickup] = useState("");
+  const [postCodes, setPostCodes] = useState<string[]>([]);
+  const pickupRef = useRef<HTMLInputElement>(null);
+  const deliveryRef = useRef<HTMLInputElement>(null);
+  const pickupAutoRef = useRef<google.maps.places.Autocomplete | null>(null);
+  const deliveryAutoRef = useRef<google.maps.places.Autocomplete | null>(null);
+
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_API_KEY || "",
+    libraries: ["places"],
+  });
+
+  const extractPostcode = (
+    place: google.maps.places.PlaceResult
+  ): string | null => {
+    const component = place.address_components?.find((c) =>
+      c.types.includes("postal_code")
+    );
+    return component?.long_name?.toUpperCase() || null;
+  };
+
+  const handlePlaceSelect = (type: "pickup" | "delivery") => {
+    const ref = type === "pickup" ? pickupAutoRef : deliveryAutoRef;
+    const place = ref.current?.getPlace();
+
+    if (!place || !place.address_components) {
+      toast.error("Please select a valid UK postcode");
+      return;
+    }
+
+    const postcode = extractPostcode(place);
+    if (!postcode) {
+      toast.error("No postcode found");
+      return;
+    }
+
+    if (type === "pickup") {
+      setPickup(postcode);
+    } else {
+      if (!isMultiple) {
+        setPostCodes([postcode]);
+      } else {
+        if (postCodes.includes(postcode)) {
+          toast.warning("This postcode is already added");
+          return;
+        }
+        setPostCodes((prev) => [...prev, postcode]);
+        if (deliveryRef.current) deliveryRef.current.value = "";
+      }
+    }
+  };
+
+  // Handle removing a postcode
+  const handleRemovePostCode = (code: string) => {
+    setPostCodes(postCodes.filter((item) => item !== code));
+  };
 
   const handleSubmit = async (data: FieldValues) => {
     const toastId = toast.loading("Uploading...");
 
-    const { toAvgDeliveryTime, ...restData } = data;
+    const { toAvgDeliveryTime, toavgCollectionTime, ...restData } = data;
 
     const date = new Date(data.preferredCollectionDate).toISOString();
 
@@ -30,10 +88,12 @@ const DeliveryForm = () => {
     try {
       const res = await delivery({
         ...restData,
+        pickupPostCode: pickup,
+        recipientPostCode: postCodes,
         preferredCollectionDate: date,
         packageWeight,
       }).unwrap();
-      console.log(res?.data?.id);
+
       if (res) {
         toast.success("Uploaded successfully", { id: toastId });
         router.push(`/checkout?price=${price}&id=${res?.data?.id}`);
@@ -45,6 +105,8 @@ const DeliveryForm = () => {
     }
   };
 
+  if (!isLoaded) return <Spinner />;
+
   return (
     <div className="max-w-3xl mx-auto md:px-5 md:my-12 my-5">
       <MyFormWrapper onSubmit={handleSubmit}>
@@ -52,15 +114,78 @@ const DeliveryForm = () => {
         <MyFormInput name="pickupName" placeholder="Name" />
         <MyFormInput name="pickupMobile" placeholder="Mobile Number" />
         <MyFormInput name="pickupEmail" placeholder="Email" />
-        <MyFormInput name="pickupPostCode" placeholder="Post Code" />
+        {/* <MyFormInput name="pickupPostCode" placeholder="Post Code" /> */}
+
+        <Autocomplete
+          onLoad={(autocomplete) => (pickupAutoRef.current = autocomplete)}
+          onPlaceChanged={() => handlePlaceSelect("pickup")}
+          options={{
+            componentRestrictions: { country: "uk" },
+            types: ["postal_code"],
+          }}
+        >
+          <input
+            ref={pickupRef}
+            name="pickup"
+            className="w-full px-4 py-3 md:text-[17px] rounded-md focus:outline-none focus:ring-2 bg-white mb-4"
+            placeholder="Post Code"
+          />
+        </Autocomplete>
+
         <MyFormInput name="pickupAddress" placeholder="Address" />
 
         <p className="text-xl font-medium mt-8 mb-3">Recipient Information</p>
         <MyFormInput name="recipientName" placeholder="Name" />
         <MyFormInput name="recipientMobile" placeholder="Mobile Number" />
         <MyFormInput name="recipientEmail" placeholder="Email" />
-        <MyFormInput name="recipientPostCode" placeholder="Post Code" />
+        {/* <MyFormInput name="recipientPostCode" placeholder="Post Code" /> */}
+        <Autocomplete
+          onLoad={(autocomplete) => (deliveryAutoRef.current = autocomplete)}
+          onPlaceChanged={() => handlePlaceSelect("delivery")}
+          options={{
+            componentRestrictions: { country: "uk" },
+            types: ["postal_code"],
+          }}
+        >
+          <input
+            ref={deliveryRef}
+            name="delivery"
+            className="w-full px-4 py-3 md:text-[17px] rounded-md focus:outline-none focus:ring-2 bg-white mb-4"
+            placeholder="Post Code"
+          />
+        </Autocomplete>
+
+        {isMultiple && (
+          <div className="flex flex-wrap gap-2 items-center mb-4">
+            {postCodes.map((item, idx) => (
+              <div
+                key={idx}
+                className="flex items-center bg-primary/15 px-2 rounded-md text-primary"
+              >
+                <span>{item}</span>
+                <button
+                  type="button"
+                  onClick={() => handleRemovePostCode(item)}
+                  className="ml-2 text-red-500"
+                >
+                  ×
+                </button>
+              </div>
+            ))}
+          </div>
+        )}
         <MyFormInput name="recipientAddress" placeholder="Address" />
+        <div className="inline-block">
+          <label className="text-sm font-medium">
+            <input
+              type="checkbox"
+              checked={isMultiple}
+              onChange={() => setIsMultiple(!isMultiple)}
+              className="mr-2"
+            />
+            Multiple Delivery Stop
+          </label>
+        </div>
 
         <p className="text-xl font-medium mt-8 mb-3">Type of Service</p>
         <MyFormSelect name="serviceType" options={serviceType} />
@@ -72,11 +197,19 @@ const DeliveryForm = () => {
           type="date"
         />
 
-        <MyFormSelect
-          name="avgCollectionTime"
-          options={timeType}
-          label="Average Collection Time"
-        />
+        <div className="flex items-center gap-2">
+          <MyFormSelect
+            name="avgCollectionTime"
+            options={timeType}
+            label="Average Collection Time"
+          />
+          <p>to</p>
+          <MyFormSelect
+            name="toavgCollectionTime"
+            options={timeType}
+            label="Average Collection Time"
+          />
+        </div>
 
         <div className="flex items-center gap-2">
           <MyFormSelect
