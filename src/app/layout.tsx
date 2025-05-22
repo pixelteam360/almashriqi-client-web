c29a265015cb8bbd9fd3a9f648bf51a43abaca2c
Shahin Alam <shahinexy@gmail.com>
2025-05-22 13:38:42 +0600

    Adds Google Ads tracking
    Adds Google Ads global site tag and conversion tracking scripts.
    
    The global site tag is added to track user behavior and measure the effectiveness of advertising campaigns.
    
    A conversion event trigger is also included to track specific actions, such as form submissions or purchases.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Lato } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
 import ReduxProvider from "@/redux/provider/ReduxProvider";
+import Script from "next/script";
 
 const lato = Lato({
   subsets: ["latin"],
@@ -21,6 +22,35 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Global site tag (gtag.js) - Google Ads */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17044587894"
+          strategy="afterInteractive"
+        />
+
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'AW-17044587894');
+          `}
+        </Script>
+
+        {/* Optional: Conversion event trigger (can also be triggered on specific actions) */}
+        <Script id="gtag-conversion" strategy="afterInteractive">
+          {`
+  gtag('event', 'conversion', {
+      'send_to': 'AW-17044587894/s7-OCLiLx8saEPaKv78_',
+      'value': 25.0,
+      'currency': 'GBP',
+      'transaction_id': ''
+  });
+          `}
+        </Script>
+      </head>
       <body className={`${lato.className} antialiased`}>
         <Toaster position="bottom-right" richColors />
         <ReduxProvider>{children}</ReduxProvider>
