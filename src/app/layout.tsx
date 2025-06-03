c1ed7a38b5d955331d5ceba2dd22300d4871c747
Shahin Alam <shahinexy@gmail.com>
2025-06-03 10:44:57 +0600

    meta-pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { Toaster } from "sonner";
 import ReduxProvider from "@/redux/provider/ReduxProvider";
 import Script from "next/script";
+import Image from "next/image";
 
 const lato = Lato({
   subsets: ["latin"],
@@ -31,29 +32,55 @@ export default function RootLayout({
 
         <Script id="gtag-init" strategy="afterInteractive">
           {`
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
 
-  gtag('config', 'AW-17044587894');
+            gtag('config', 'AW-17044587894');
           `}
         </Script>
 
         {/* Optional: Conversion event trigger (can also be triggered on specific actions) */}
         <Script id="gtag-conversion" strategy="afterInteractive">
           {`
-  gtag('event', 'conversion', {
-      'send_to': 'AW-17044587894/s7-OCLiLx8saEPaKv78_',
-      'value': 25.0,
-      'currency': 'GBP',
-      'transaction_id': ''
-  });
+            gtag('event', 'conversion', {
+                'send_to': 'AW-17044587894/s7-OCLiLx8saEPaKv78_',
+                'value': 25.0,
+                'currency': 'GBP',
+                'transaction_id': ''
+            });
+          `}
+        </Script>
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1211688703956465');
+            fbq('track', 'PageView');
           `}
         </Script>
       </head>
       <body className={`${lato.className} antialiased`}>
         <Toaster position="bottom-right" richColors />
-        <ReduxProvider>{children}</ReduxProvider>
+        <ReduxProvider>
+          {" "}
+          <noscript>
+            <Image
+              height="1"
+              width="1"
+              style={{ display: "none" }}
+              src="https://www.facebook.com/tr?id=1211688703956465&ev=PageView&noscript=1"
+              alt=""
+            />
+          </noscript>
+          {children}
+        </ReduxProvider>
       </body>
     </html>
   );
